df213e7d756b18451e0e1af652c33d8f3046d764
Andrii Solohub <andrei.sologub12@gmail.com>
2022-11-22 16:17:11 +0200

    dynemic import instead common import

--- hw_9/schedule/index.js
+++ hw_9/schedule/index.js
@@ -1,5 +1,5 @@
 import readline from 'readline';
-import { schedule } from './schedule.js';
+// import { schedule } from './schedule.js';
 
 async function readTerminalInput() {
   const rl = readline.createInterface({
@@ -11,9 +11,16 @@ async function readTerminalInput() {
   rl.prompt();
 
   for await (const line of rl) {
-    console.log(
-      schedule.find((daySchedule) => daySchedule.day === line.toLowerCase()),
-    );
+    import('./schedule.js')
+      .then((data) =>
+        console.log(
+          data.schedule.find(
+            (daySchedule) => daySchedule.day === line.toLowerCase(),
+          ),
+        ),
+      )
+      .catch((e) => console.log(e));
+
     console.log('Press ctrl+c to exit');
     rl.prompt();
   }
