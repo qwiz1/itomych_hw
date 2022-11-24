e1d6a8813a595a27bcda15f5cad2f9e27db1b59e
Andrii Solohub <andrei.sologub12@gmail.com>
2022-11-24 17:15:55 +0200

    execute isInside for tests

--- TS_Module/hw_1/src/index.ts
+++ TS_Module/hw_1/src/index.ts
@@ -1,3 +1,29 @@
 import { productOfTwoPolynomials } from './task_2/product_polynomials';
+import { isInside } from './task_3/triangle';
 
 console.log(productOfTwoPolynomials('2x - 2', '3x + 1'));
+
+console.log(
+  `Is point lies inside the triangle? ${isInside(
+    10,
+    10,
+    15,
+    40,
+    30,
+    10,
+    15,
+    50,
+  )}`,
+);
+console.log(
+  `Is point lies inside the triangle? ${isInside(
+    10,
+    10,
+    15,
+    40,
+    30,
+    10,
+    15,
+    10,
+  )}`,
+);
